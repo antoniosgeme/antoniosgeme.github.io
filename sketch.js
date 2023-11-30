837e1514c542ccbefb7522628c8b4162f4fa427b
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2023-11-30 14:15:30 -0500

    update

--- sketch.js
+++ sketch.js
@@ -1,46 +1,90 @@
-// Variables for wing simulation
-let chord;
-let wingLength;
-let angleOfAttack = 0;
-let liftForce = 0;
-let dragForce = 0;
+let circulation = 1; // Initial circulation strength
+let maxCirculation = 10; // Maximum circulation strength
+let wing;
 
 function setup() {
-  createCanvas(400, 400);
-  wingLength = height * 0.6; // Adjust the length of the wing here
-  chord = wingLength * 0.1; // Adjust the chord length of the wing here
+  createCanvas(600, 400);
+  circulationSlider = createSlider(0, maxCirculation, circulation, 0.1);
+  circulationSlider.position(20, height + 20);
+  circulationSlider.style('width', '200px');
+
+  wing = createWing(width / 2, height / 2); // Create a wing object
 }
 
 function draw() {
-  background(220);
-  translate(width / 2, height / 2);
+  background(255);
 
-  // Wing representation
-  strokeWeight(2);
-  line(-wingLength / 2, 0, wingLength / 2, 0);
-  line(-wingLength / 2, chord / 2, -wingLength / 2, -chord / 2);
-  line(-wingLength / 2, 0, -wingLength / 2 + 10, 0);
-  line(wingLength / 2, 0, wingLength / 2 - 10, 0);
+  // Get circulation value from the slider
+  circulation = circulationSlider.value();
 
-  // Airflow visualization
-  let airflowSpeed = 50; // Change this value to adjust the speed of airflow
-  for (let x = -width / 2; x < width / 2; x += 10) {
-    let y1 = 10 * sin(angleOfAttack) * sin(PI * x / wingLength);
-    let y2 = 10 * sin(angleOfAttack) * sin(PI * (x + 10) / wingLength);
-    line(x, y1, x + 10, y2);
-  }
+  // Update wing circulation
+  wing.setCirculation(circulation);
 
-  // Forces calculation (basic potential flow approximation)
-  let velocity = airflowSpeed * cos(angleOfAttack);
-  liftForce = 0.5 * 1.225 * velocity * velocity * chord * sin(angleOfAttack);
-  dragForce = 0.5 * 1.225 * velocity * velocity * chord * cos(angleOfAttack);
-
-  // Display forces
-  fill(0);
-  text(`Lift: ${liftForce.toFixed(2)} N`, -width / 2 + 10, 20);
-  text(`Drag: ${dragForce.toFixed(2)} N`, -width / 2 + 10, 40);
+  // Calculate and display the resulting flow field
+  calculateFlowField();
+  displayFlowField();
+  wing.display();
 }
 
-function mouseDragged() {
-  angleOfAttack = map(mouseX, 0, width, -PI / 6, PI / 6); // Adjust the range of the angle of attack
+function createWing(x, y) {
+  let wing = {
+    x: x,
+    y: y,
+    circulation: circulation,
+    setCirculation: function(c) {
+      this.circulation = c;
+    },
+    display: function() {
+      // Draw your wing or shape here using p5.js drawing functions
+      // For example, you can draw a simple wing shape using beginShape(), vertex(), etc.
+      // Replace this with your own wing drawing code
+      fill(100);
+      beginShape();
+      vertex(this.x - 50, this.y);
+      vertex(this.x + 50, this.y);
+      vertex(this.x, this.y - 100);
+      endShape(CLOSE);
+    }
+  };
+  return wing;
+}
+
+function calculateFlowField() {
+  // Calculate the flow field based on potential flow equations
+  // Use the circulation value to modify the flow field around the wing
+  // This example uses a simple vortex flow calculation
+
+  // Loop through each pixel in the canvas
+  for (let x = 0; x < width; x += 10) {
+    for (let y = 0; y < height; y += 10) {
+      let velocity = calculateVortexVelocity(x, y, wing);
+      // Draw vectors to represent velocity
+      drawVector(x, y, velocity);
+    }
+  }
+}
+
+function calculateVortexVelocity(x, y, wing) {
+  // Calculate velocity induced by a vortex at position (x, y)
+  let distance = dist(x, y, wing.x, wing.y);
+  let angle = atan2(y - wing.y, x - wing.x);
+  let velocityMagnitude = wing.circulation / (2 * PI * distance);
+  let vx = velocityMagnitude * sin(angle);
+  let vy = -velocityMagnitude * cos(angle);
+  return createVector(vx, vy);
+}
+
+function drawVector(x, y, vector) {
+  // Draw a line to represent the velocity vector at position (x, y)
+  let arrowSize = 5;
+  stroke(0);
+  strokeWeight(1);
+  line(x, y, x + vector.x, y + vector.y);
+  push();
+  translate(x + vector.x, y + vector.y);
+  rotate(vector.heading());
+  let arrowSizeAdjusted = arrowSize / 2;
+  line(0, 0, -arrowSize, -arrowSizeAdjusted);
+  line(0, 0, -arrowSize, arrowSizeAdjusted);
+  pop();
 }

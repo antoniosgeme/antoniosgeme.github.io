788ff0f12802976122515d38bb21ec5d34c326ca
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2023-11-30 13:53:46 -0500

    update

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400); // Creates a canvas of 400x400 pixels
+  }
+  
+  function draw() {
+    background(220); // Sets the background color to light gray
+    fill(255, 0, 0); // Sets the fill color to red
+    ellipse(200, 200, 50, 50); // Draws a red circle at coordinates (200, 200)
+  }
+  

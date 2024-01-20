02672135955aadcb6f807f87a8c801ab70119b2c
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2024-01-20 17:28:39 -0500

    update

--- _pages/about.md
+++ _pages/about.md
@@ -8,7 +8,8 @@ redirect_from:
   - /about.html
 ---
 
-Greetings! I am a PhD Candidate pursuing my studies in the Department of Aerospace Engineering at the University of Maryland, where I'm fortunate to conduct research under the guidance of [Dr. Anya Jones](https://aero.umd.edu/clark/faculty/40/Anya-R-Jones) in the [Separated and Transient Aerodynamics Laboratory (STAL)](http://stal.umd.edu/). My research revolves around the realms of unsteady aerodynamics and control theory, and is aimed at addressing challenges across fields such as aerospace engineering and renewable energy. 
+Greetings! I am a PhD Candidate in the Department of Aerospace Engineering at the University of Maryland, where I'm fortunate to conduct research under the guidance of [Dr. Anya Jones](https://aero.umd.edu/clark/faculty/40/Anya-R-Jones) in the [Separated and Transient Aerodynamics Laboratory (STAL)](http://stal.umd.edu/). My research revolves around the realms of unsteady aerodynamics, control theory, and optimization and is primarly aimed at addressing challenges in the aerospace engineering and renewable energy sectors. 
 
+Previously, I earned a Bachelor's in mechanical engineering from New York University and a Master's in aeronuatical engineering from the University of Cambridge. I have worked on a diverse set of problems, ranging from biomimetic robotics to combustion thermoacoustics. All of them however, have followed a common premise: understanding and manipulating complex fluid behavior and its interaction with engineering systems. 
 
 
--- update_publications.bat
+++ update_publications.bat
@@ -1,11 +1,16 @@
 @echo off
 
-echo Extracting and processing references from google scholar...
-call python assets\python\export_google_scholar.py
+REM This file is used to update the publications part of my CV and the publications section of my website
+REM assets/CV/references.bib must be updated (exporting from google scholar gets blocked too often)
+REM echo Extracting and processing references from google scholar...
+REM call python assets\python\export_google_scholar.py
+
+REM This function recognizes APS abstracts and add a keyword "abstract" to be recoognized by the other programs
 call python assets\python\bib_process.py
 
+REM This function turns the bib file to a yml file to be read by the website
 echo Updating YAML file...
-REM call python assets\python\bib_to_yml.py
+call python assets\python\bib_to_yml.py
 
 REM Specify the full or relative path to your LaTeX CV document (including the .tex extension)
 SET CV_DOCUMENT_PATH=assets\CV\CV_AntoniosG.tex
@@ -29,11 +34,7 @@ biber %CV_DOCUMENT% >NUL
 REM Run pdflatex again to resolve references
 pdflatex %CV_DOCUMENT%.tex >NUL
 pdflatex %CV_DOCUMENT%.tex >NUL
-REM Cleanup unnecessary files (optional)
-del %CV_DOCUMENT%.aux %CV_DOCUMENT%.bbl  %CV_DOCUMENT%.log %CV_DOCUMENT%.out %CV_DOCUMENT%.run.xml %CV_DOCUMENT%.bcf
-REM %CV_DOCUMENT%.blg
-REM Open the PDF in your default PDF viewer (optional)
-REM Uncomment the following line if you want to automatically open the PDF after compilation
-REM start "" %CV_DOCUMENT%.pdf
+REM Cleanup unnecessary files 
+del %CV_DOCUMENT%.aux %CV_DOCUMENT%.bbl  %CV_DOCUMENT%.log %CV_DOCUMENT%.out %CV_DOCUMENT%.run.xml %CV_DOCUMENT%.bcf %CV_DOCUMENT%.blg
 
 echo Compilation completed.
